9fddfc026f56011935446eab120213328c6ae961
Haven <513433750@qq.com>
2017-07-11 16:25:32 +0800

    Initial Commit

---
+++ MVVM/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  MVVM
+//
+//  Created by Haven on 2017/7/11.
+//  Copyright © 2017年 com.RuanZhiHongYi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
